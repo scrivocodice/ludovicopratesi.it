31e9d4c4fef252575d6601b4d4bc7b16d1905ef5
Scrivocodice <info@scrivocodice.it>
2026-04-04 06:12:25 +0200

    Rimuove cartelle non più utilizzate.

--- etc/gunicorn/www.py
+++
@@ -1,6 +0,0 @@
-import os
-
-bind = "unix:////home/ludovicopratesi/var/run/gunicorn.sock"
-workers = (os.sysconf("SC_NPROCESSORS_ONLN") * 2) + 1
-loglevel = "error"
-proc_name = "ludovicopratesi.it"
